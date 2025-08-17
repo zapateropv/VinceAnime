ac98540f7f6d1f208929fe21b366f9f270457bb2
zapateropv <166297524+zapateropv@users.noreply.github.com>
2025-08-17 19:28:30 +0800

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwind from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),tailwind()],
+  base: process.env.VITE_BASE_PATH || '/VinceAnime'
 })
